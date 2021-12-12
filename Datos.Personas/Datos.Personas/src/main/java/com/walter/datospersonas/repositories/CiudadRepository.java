a47f30ed22f34b200ce68fe76e0ffc5c7cc21e68
Walter Gomez <62272491+wgekko@users.noreply.github.com>
2021-12-12 17:24:09 -0300

    Add files via upload

---
+++ Datos.Personas/Datos.Personas/src/main/java/com/walter/datospersonas/repositories/CiudadRepository.java
@@ -0,0 +1,18 @@
+package com.walter.datospersonas.repositories;
+
+import com.walter.datospersonas.entidades.Ciudad;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CiudadRepository extends JpaRepository<Ciudad, String> {
+
+  @Query("select p from Ciudad p where p.nombre LIKE :q")
+  List<Ciudad> findAll(@Param("q") String q);
+  
+   
+}
---
+++ Datos.Personas/Datos.Personas/src/test/java/com/walter/datospersonas/EjemplopersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.datospersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplopersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
